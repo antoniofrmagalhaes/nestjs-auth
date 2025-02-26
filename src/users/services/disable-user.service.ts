48a3897258eb6300574b7fe84b3a61d11babbfc7
Antonio Magalhães <antoniofmagalhaes@protonmail.com>
2025-02-26 15:55:57 -0400

    feat(users): add DisableUserService

---
+++ src/users/services/disable-user.service.ts
@@ -0,0 +1,24 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from '../entities/user.entity';
+
+@Injectable()
+export class DisableUserService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+
+  async execute(id: number): Promise<User> {
+    const user = await this.userRepository.findOne({ where: { id } });
+
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+
+    user.active = false;
+
+    return this.userRepository.save(user);
+  }
+}
