d162a9fef08b0d5f1696fb112b4324978fc841b5
Antonio Magalhães <antoniofmagalhaes@protonmail.com>
2025-02-28 04:06:04 -0400

    chore(auth): register CacheManager and RefreshSessionService

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,14 @@
+import { CacheModule, CacheModuleOptions } from '@nestjs/cache-manager';
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+import redisStore from 'cache-manager-redis-store';
+
 import { User } from '../users/entities/user.entity';
 import { SessionsController } from './controllers/sessions.controller';
 import { CreateSessionService } from './services/create-session.service';
+import { RefreshSessionService } from './services/refresh-session.service';
 import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
@@ -14,8 +18,20 @@ import { JwtStrategy } from './strategies/jwt.strategy';
       secret: process.env.JWT_SECRET || 'default-secret',
       signOptions: { expiresIn: '1h' },
     }),
+    CacheModule.registerAsync<CacheModuleOptions>({
+      useFactory: async (): Promise<CacheModuleOptions> => ({
+        store: redisStore,
+        socket: {
+          host: process.env.REDIS_HOST || 'localhost',
+          port: parseInt(process.env.REDIS_PORT, 10) || 6379,
+        },
+        username: process.env.REDIS_USERNAME || 'redis',
+        password: process.env.REDIS_PASSWORD || 'redis',
+        ttl: 604800,
+      }),
+    }),
   ],
   controllers: [SessionsController],
-  providers: [CreateSessionService, JwtStrategy],
+  providers: [CreateSessionService, RefreshSessionService, JwtStrategy],
 })
 export class AuthModule {}
