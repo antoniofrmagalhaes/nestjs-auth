9beb214ce0810d44a426e02876ddbd7515c09a4a
Antonio Magalhães <antoniofmagalhaes@protonmail.com>
2025-02-28 04:06:21 -0400

    feat(auth): add refresh session route to SessionsController

--- src/auth/controllers/sessions.controller.ts
+++ src/auth/controllers/sessions.controller.ts
@@ -1,18 +1,29 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, UseGuards, Request } from '@nestjs/common';
 
+import { CreateSessionDto } from '../dto/create-session.dto';
+import { RefreshSessionDto } from '../dto/refresh-session.dto';
+import { JwtAuthGuard } from '../guards/jwt-auth.guard';
 import { CreateSessionService } from '../services/create-session.service';
-
-interface CreateSessionDto {
-  email: string;
-  password: string;
-}
+import { RefreshSessionService } from '../services/refresh-session.service';
 
 @Controller('session')
 export class SessionsController {
-  constructor(private readonly createSessionService: CreateSessionService) {}
+  constructor(
+    private readonly createSessionService: CreateSessionService,
+    private readonly refreshSessionService: RefreshSessionService,
+  ) {}
 
   @Post('create')
   async createSession(@Body() createSessionDto: CreateSessionDto) {
     return this.createSessionService.execute(createSessionDto);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Post('refresh')
+  async refreshAccessToken(
+    @Request() req,
+    @Body() { refreshToken }: RefreshSessionDto,
+  ) {
+    return this.refreshSessionService.execute(req.user, refreshToken);
   }
 }
